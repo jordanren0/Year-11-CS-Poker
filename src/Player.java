421a88c8b4ddb8a9389b32daaf31a45fbc4c6a19
jordanren0 <123038714+jordanren0@users.noreply.github.com>
2023-05-23 18:16:11 +0800

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Player {
+import java.util.Random;
+public class Player {
+    private String name;
+    private int chips;
+    public Player(String name, int chips) {
+        this.name = name;
+        this.chips = chips;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChips() {
+        return chips;
+    }
+
+    public void setChips(int chips) {
+        this.chips = chips;
+    }
+
+    public void addChips(int amount) {
+        chips += amount;
+    }
+
+    public void removeChips(int amount) {
+        chips -= amount;
+    }
+
+    public void humanBet(int bet){
+        chips -= bet;
+    }
+
+    public int botBet(int range){
+        Random rand = new Random();
+        return rand.nextInt((chips - range + 1)/2) + range;
+    }
+
 }
